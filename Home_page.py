f1e4d9ca29b2255421b57dc4b48e6462e64b0c8b
Rafael <97483718+RafaelSilva89@users.noreply.github.com>
2024-03-29 14:16:45 -0300

    Add files via upload
    DetranGPT

---
+++ Home_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="🤖",
+)
+
+st.title("Bem-vindo ao Aplicativo DetranGPT 💬")
+st.header("Este aplicativo foi desenvolvido para apresentar resultados usando Linguagem Natural. 📱\n\nNota: Especificar o nome da coluna pode otimizar os resultados.\n\nEsta é uma versão Beta.")
